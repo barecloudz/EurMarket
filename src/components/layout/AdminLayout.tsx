2c033e6230b32e755bd35820308973e794e50c6e
barecloudz <barecloudz@gmail.com>
2026-03-09 23:22:04 -0400

    Fix admin layout white background

--- src/components/layout/AdminLayout.tsx
+++ src/components/layout/AdminLayout.tsx
@@ -3,7 +3,7 @@ import AdminSidebar from './AdminSidebar';
 
 export default function AdminLayout() {
   return (
-    <div className="min-h-screen flex bg-brand-black">
+    <div className="min-h-screen flex" style={{ backgroundColor: 'var(--color-background)' }}>
       <AdminSidebar />
       <main className="flex-1 pt-[72px] md:pt-8 px-4 pb-6 md:px-8 overflow-auto">
         <Outlet />
